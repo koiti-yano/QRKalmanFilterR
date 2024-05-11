f6f14ebbb44aec915853836d1c000be2c1504bc0
koiti-yano <koiti@komazawa-u.ac.jp>
2024-05-11 11:24:24 +0900

    Rev

--- R/kalman_filter_recursion.R
+++ R/kalman_filter_recursion.R
@@ -54,7 +54,7 @@
 
 kalman_filter_recursion <- function(y, u, x0, P0, F, E, H,
                                     V, W) {
-  source("./kalman_filter.R")
+  #source("./kalman_filter.R")
   yy<-y # variable traceability
   uu <- u # variable traceability
   FF <- F # variable traceability
